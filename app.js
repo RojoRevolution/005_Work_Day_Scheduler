2c671cfbe44a73b2a183111cb9cac1c4bd6c0a4c
David Rojo <rojo@rojorevolution.com>
2021-01-09 00:07:40 -0600

    working on a for loop to dynamically add all content but having issues with the rows not rendering

--- app.js
+++ app.js
@@ -17,19 +17,34 @@ $(document).ready(function () {
         complete: momentJS.format('LLLL')
     };
 
-    console.log(current.day);
-    console.log(current.month);
-    console.log(current.dayOfMonth);
-    console.log(current.year);
-    console.log(`${current.hour}:${current.minutes} ${current.amPM}`)
-    console.log(current.complete)
+    //hours var was set manually until I can figure out if a simialr array can be pulled from api
+    var hours = ['9:00', '10:00', '11:00', '12:00', '1:00', '2:00', '3:00', '4:00', '5:00'];
+
+    console.log(current.day)
+
+    var container = $('.container')
+    var row = $('.row')
+    var hourDiv = /*html*/`<div class="col-2 p-3 hour">${hours[0]}</div>`;
+    var textArea = /*html*/`<textarea class="col - 8 p - 3 description future"></textarea>`;
+    var saveBtn = /*html*/ `<button class="col-1 saveBtn"><i class="far fa-save"></i></button>`;
 
 
 
+
+    //Append Row to Container
+    //Then appent content to row
 
     var currentDayEl = $('#currentDay');
     currentDayEl.text(`${current.day} ${current.month} ${current.dayOfMonth} ${current.year}`)
 
+    function renderToDo() {
+        for (var i = 0; i <= hours.length; i++) {
+            container.append(row);
+            row.append(hourDiv, textArea, saveBtn);
+        }
+    }
+
+    renderToDo();
 
 
 
