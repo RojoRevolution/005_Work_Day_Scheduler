b595df797f574d46173a01047e8b01e2ccefade5
David Rojo <rojo@rojorevolution.com>
2021-01-09 00:33:44 -0600

    included all html in onr variable, no trying to figure out why my for loop is not rendering the correct time

--- app.js
+++ app.js
@@ -18,15 +18,24 @@ $(document).ready(function () {
     };
 
     //hours var was set manually until I can figure out if a simialr array can be pulled from api
-    var hours = ['9:00', '10:00', '11:00', '12:00', '1:00', '2:00', '3:00', '4:00', '5:00'];
+    var $hourList = ['9:00', '10:00', '11:00', '12:00', '1:00', '2:00', '3:00', '4:00', '5:00'];
 
     console.log(current.day)
 
     var container = $('.container')
-    var row = $('.row')
-    var hourDiv = /*html*/`<div class="col-2 p-3 hour">${hours[0]}</div>`;
-    var textArea = /*html*/`<textarea class="col - 8 p - 3 description future"></textarea>`;
-    var saveBtn = /*html*/ `<button class="col-1 saveBtn"><i class="far fa-save"></i></button>`;
+    var hourDisplay = document.querySelector(".hour");
+    // var row = $('<div class="row">');
+    // var hourDiv = /*html*/`<div class="col-2 p-3 hour">${hourList[0]}</div>`;
+    // var textArea = /*html*/`<textarea class="col - 8 p - 3 description future"></textarea>`;
+    // var saveBtn = /*html*/ `<button class="col-1 saveBtn"><i class="far fa-save"></i></button>`;
+    // var bR = /*html*/`<br>`;
+
+    var content = /*html*/`
+    <div class="row">
+    <div class="col-2 p-3 hour"></div>
+    <textarea class="col - 8 p - 3 description future"></textarea>
+    <button class="col-1 saveBtn"><i class="far fa-save"></i></button>
+    </div >`;
 
 
 
@@ -35,12 +44,13 @@ $(document).ready(function () {
     //Then appent content to row
 
     var currentDayEl = $('#currentDay');
-    currentDayEl.text(`${current.day} ${current.month} ${current.dayOfMonth} ${current.year}`)
+    currentDayEl.text(`${current.day} ${current.month} ${current.dayOfMonth} ${current.year} `)
 
     function renderToDo() {
-        for (var i = 0; i <= hours.length; i++) {
-            container.append(row);
-            row.append(hourDiv, textArea, saveBtn);
+        for (var i = 0; i < $hourList.length; i++) {
+            container.append(content);
+            console.log($hourList[i])
+            $('.hour').text($hourList[i]);
         }
     }
 
