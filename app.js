73780fe309f8d3d2e6a8135d4ca4b46e4bfdec8f
David Rojo <rojo@rojorevolution.com>
2021-01-09 21:09:03 -0600

    calling it a night, need to figure out why items are not rendering after being pulled from local storage

--- app.js
+++ app.js
@@ -22,10 +22,10 @@ $(document).ready(function () {
     var currentDayEl = $('#currentDay');
     currentDayEl.text(`${current.day} ${current.month} ${current.dayOfMonth} ${current.year} `)
 
-    var currentHour = current.hour
-    console.log(`${current.hour}`)
+    // var currentHour = current.hour
+
     var currentHour24 = current.hour24;
-    console.log(`${currentHour24}`)
+
 
     //hours var was set manually until I can figure out if a similar array can be pulled from api
     var hourList = ['9:00', '10:00', '11:00', '12:00', '1:00', '2:00', '3:00', '4:00', '5:00'];
@@ -42,16 +42,20 @@ $(document).ready(function () {
             var content = /*html*/`
             <div class="row">
                 <div class="col-2 p-3 hour" value="${hourList24[i]}">${hourList[i]}</div>
-                <textarea class="col - 8 p - 3 description" value="${i}"></textarea>
+                <textarea id="${i}" class="col - 8 p - 3 description" value="${i}"></textarea>
                 <button class="col-1 saveBtn" value="${i}"><i class="far fa-save"></i></button>
             </div >`;
             container.append(content);
+            var getItemIterator = `ToDo${i}`
             var descriptionBox = $(this).parent().find(".description")
-            var getToDo = localStorage.getItem("ToDo");
+            console.log(descriptionBox)
+            var getToDo = localStorage.getItem(getItemIterator);
             var parseToDo = JSON.parse(getToDo);
+            // Parsed Items log but will not render to description box
+            console.log(parseToDo)
 
-            $(".description").text(parseToDo)
-            descriptionBox.text(getToDo)
+            // $(".description").text(parseToDo)
+            descriptionBox.text(parseToDo)
         }
 
 
@@ -64,12 +68,12 @@ $(document).ready(function () {
     function click() {
         $(".saveBtn").on("click", function (event) {
             event.preventDefault();
-            // var time = $(this).parent().find(".hour").text();
+            // vars use $(this) for where the click happens, goes up to parent(row), and finds the specified class and gets it's value 
             var btnValue = $(this).parent().find(".saveBtn").val();
             var text = $(this).parent().find(".description").val();
-            toDoList.push(text);
+            // toDoList.push(text);
             // adding + btnValue after the key includes it in the key
-            localStorage.setItem("ToDo-" + btnValue, JSON.stringify(text));
+            localStorage.setItem("ToDo" + btnValue, JSON.stringify(text));
 
 
         });
@@ -97,6 +101,8 @@ $(document).ready(function () {
 
     renderToDo();
 });
+
+var storageArray = [];
 
 
 
