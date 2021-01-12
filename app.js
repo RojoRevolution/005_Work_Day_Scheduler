0a645a796e52b0d5566c5e173e4a9b077ea8f083
David Rojo <rojo@rojorevolution.com>
2021-01-11 18:39:42 -0600

    pushing for live testing, project should be complete

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
 
+    //*************************//
+    // Moment JS API
+    //*************************//
     // gets entire moment object
     var momentJS = moment();
     //currentStates pulls formated version of the current date
@@ -16,81 +19,57 @@ $(document).ready(function () {
         complete: momentJS.format('LLLL')
     };
 
-
-
-    //==================================
-    // Variable appends to header to display current date
+    //*************************//
+    // Variables
+    //*************************//
+    // Variable CurrentDay ELappends to header to display current date
     var currentDayEl = $('#currentDay');
     currentDayEl.text(`${current.day} ${current.month} ${current.dayOfMonth} ${current.year} `)
 
-    // var currentHour = current.hour
-
+    //currentHour grabs the current hour in 24Hour(Military) Format
     var currentHour24 = current.hour24;
-    console.log("Current Hour: " + currentHour24)
-    var hourTest = 13;
 
-
-    //hours var was set manually until I can figure out if a similar array can be pulled from api
-    var hourList = ['9:00', '10:00', '11:00', '12:00', '1:00', '2:00', '3:00', '4:00', '5:00'];
-    var hourList24 = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+    //hour list var are used for ids / data / text in the dynamic content
+    var hourList = ['8:00', '9:00', '10:00', '11:00', '12:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', '8:00'];
+    var hourList24 = ['08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'];
+    //container selects the div we will append content to
     var container = $('.container')
 
 
 
+    //*************************//
+    // Functions
+    //*************************//
     //================================
     //Get Items from Local storage
     function getItems(iterator) {
         var getItemIterator = `ToDo${iterator}`
         var getToDo = localStorage.getItem(getItemIterator);
-        // console.log(getToDo)
-        //Add text to textbox based on iteration
         $("#text" + iterator).text(getToDo)
     };
 
     //================================
     //BG Color Function
-    // Using hourTest which is set to a single number seems to work just fine
-    // function bgColor() {
-    //     $(".hour").each(function () {
-    //         console.log($(this).attr("data-hour") + " | " + currentHour24);
-    //         if ($(this).attr("data-hour") < hourTest) {
-    //             $(this).siblings(".description").addClass("past")
-    //             console.log("Past");
-    //         }
-    //         if ($(this).attr("data-hour") == hourTest) {
-    //             $(this).siblings(".description").addClass("present")
-    //             console.log("Present");
-    //         }
-    //         if ($(this).attr("data-hour") > hourTest) {
-    //             $(this).siblings(".description").addClass("future")
-    //             console.log("future");
-    //         }
-    //     });
-    // }
-
-    // using the current hour from Moment JS does not work the same
     function bgColor() {
         $(".hour").each(function () {
             console.log($(this).attr("data-hour") + " | " + currentHour24);
             if ($(this).attr("data-hour") < currentHour24) {
                 $(this).siblings(".description").addClass("past")
-                console.log("Past");
+
             }
             if ($(this).attr("data-hour") == currentHour24) {
                 $(this).siblings(".description").addClass("present")
-                console.log("Present");
+
             }
             if ($(this).attr("data-hour") > currentHour24) {
                 $(this).siblings(".description").addClass("future")
-                console.log("future");
+
             }
         });
     }
 
-
-
     //================================
-    // Renders all HTML content
+    // Renders all HTML content on load / reload
     function renderToDo() {
         //For loop will create and set each row of content
         for (let i = 0; i < hourList.length; i++) {
@@ -98,15 +77,18 @@ $(document).ready(function () {
             var content = /*html*/`
             <div class="row">
                 <div id="${hourList[i]}" class="col-2 p-3 hour" data-hour="${hourList24[i]}">${hourList[i]}</div>
-                <textarea id="toDo" class="col - 8 p - 3 description" value="${i}" ></textarea>
+                <textarea id="text${i}" class="col - 8 p - 3 description" value="${i}" ></textarea>
                 <button class="col-1 saveBtn" value="${i}"><i class="far fa-save"></i></button>
             </div >`;
             // Append HTML content to page
             container.append(content);
+            //Calls the function that gets items from localStorage
             getItems(i)
 
         }
+        //Calls function that sets the BG color for each text area
         bgColor()
+
         click()
 
     }
@@ -126,10 +108,7 @@ $(document).ready(function () {
         });
     }
 
-
-
-
-
+    //Renders Content on initial page load
     renderToDo();
 });
 
